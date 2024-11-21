9cb5f1b901ef4afeb4a69ebcc76eeef038ee7040
Suzane Hamze <suzane.hamze@epinova.no>
2024-11-21 14:20:43 +0100

    long to message

---
+++ bytesAndBigIntegers.py
@@ -0,0 +1,12 @@
+import sys
+import base64
+from Crypto.Util.number import long_to_bytes
+
+
+ords = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+
+#long to bytes
+message_bytes = long_to_bytes(ords)
+
+#print message
+print(message_bytes)
